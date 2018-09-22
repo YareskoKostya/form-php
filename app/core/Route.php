78af4fd7959be2154e5509d2cfc33210d2787fe9
YareskoKostya <zhtcmrj@gmail.com>
2018-09-22 09:13:43 +0300

    ch route: translate comments, ch names of classes

--- app/core/Route.php
+++ app/core/Route.php
@@ -5,28 +5,28 @@ namespace App\Core;
 class Route {
 
     static function start() {
-        // контроллер и действие по умолчанию
+        // controller and default action
         $controller_name = 'Main';
         $action_name = 'index';
 
         $routes = explode('/', $_SERVER['REQUEST_URI']);
 
-        // получаем имя контроллера
+        // get the name of the controller
         if (!empty($routes[1])) {
             $controller_name = $routes[1];
         }
 
-        // получаем имя экшена
+        // get the name of the action
         if (!empty($routes[2])) {
             $action_name = $routes[2];
         }
 
-        // добавляем префиксы
-        $controller_name = 'Controller_' . $controller_name;
-        $action_name = 'action_' . $action_name;
+        // add prefixes
+        $controller_name = 'Controller' . $controller_name;
+        $action_name = 'action' . $action_name;
 
-        // подцепляем файл с классом контроллера
-        $controller_file = strtolower($controller_name) . '.php';
+        // take file with controller class
+        $controller_file = $controller_name . '.php';
         $controller_path = "app/controllers/" . $controller_file;
         if (file_exists($controller_path)) {
             include "app/controllers/" . $controller_file;
@@ -38,13 +38,13 @@ class Route {
             Route::ErrorPage404();
         }
 
-        // создаем контроллер
+        // create a controller
         $controller_name = "app\\controllers\\" . $controller_name;
         $controller = new $controller_name;
         $action = $action_name;
 
         if (method_exists($controller, $action)) {
-            // вызываем действие контроллера
+            // call the controller action
             $controller->$action();
         } else {
             // здесь также разумнее было бы кинуть исключение
