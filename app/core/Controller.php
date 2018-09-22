bb9b8b866720a4f2c2b3b72e5b264584c99b6ae1
YareskoKostya <zhtcmrj@gmail.com>
2018-09-22 09:55:53 +0300

    add controller

---
+++ app/core/Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace application\core;
+use application\core\View;
+
+class Controller {
+
+    public $model;
+    public $view;
+
+    function __construct() {
+        $this->view = new View();
+    }
+
+    function action_index() {
+        
+    }
+
+}
