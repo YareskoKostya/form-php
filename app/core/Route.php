937847b4633644f0e0ba1ebc52a7cfd76348534e
YareskoKostya <zhtcmrj@gmail.com>
2018-09-22 09:01:25 +0300

    add route

---
+++ app/core/Route.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Core;
+
+class Route {
+
+    static function start() {
+        // контроллер и действие по умолчанию
+        $controller_name = 'Main';
+        $action_name = 'index';
+
+        $routes = explode('/', $_SERVER['REQUEST_URI']);
+
+        // получаем имя контроллера
+        if (!empty($routes[1])) {
+            $controller_name = $routes[1];
+        }
+
+        // получаем имя экшена
+        if (!empty($routes[2])) {
+            $action_name = $routes[2];
+        }
+
+        // добавляем префиксы
+        $controller_name = 'Controller_' . $controller_name;
+        $action_name = 'action_' . $action_name;
+
+        // подцепляем файл с классом контроллера
+        $controller_file = strtolower($controller_name) . '.php';
+        $controller_path = "app/controllers/" . $controller_file;
+        if (file_exists($controller_path)) {
+            include "app/controllers/" . $controller_file;
+        } else {
+            /*
+              правильно было бы кинуть здесь исключение,
+              но для упрощения сразу сделаем редирект на страницу 404
+             */
+            Route::ErrorPage404();
+        }
+
+        // создаем контроллер
+        $controller_name = "app\\controllers\\" . $controller_name;
+        $controller = new $controller_name;
+        $action = $action_name;
+
+        if (method_exists($controller, $action)) {
+            // вызываем действие контроллера
+            $controller->$action();
+        } else {
+            // здесь также разумнее было бы кинуть исключение
+            Route::ErrorPage404();
+        }
+    }
+
+    function ErrorPage404() {
+        $host = 'http://' . $_SERVER['HTTP_HOST'] . '/';
+        header('HTTP/1.1 404 Not Found');
+        header("Status: 404 Not Found");
+        header('Location:' . $host . '404');
+    }
+
+}
