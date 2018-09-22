ca83049f7d1006aa6fef86feff023126b3f803a8
YareskoKostya <zhtcmrj@gmail.com>
2018-09-22 09:00:12 +0300

    add sample config

---
+++ files/config.sample.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Костя
+ * Date: 20.09.2018
+ * Time: 19:02
+ */
+
+$settings  = [
+    'host' => 'localhost',
+    'port' => '3306',
+    'dbname' => 'resume_db',
+    'username' => '',
+    'password' => '',
+    'charset' => 'utf8'
+];
