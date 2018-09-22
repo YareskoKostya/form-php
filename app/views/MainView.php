5922226a690ef17b1e321ea842073f786a102f14
YareskoKostya <zhtcmrj@gmail.com>
2018-09-22 14:49:57 +0300

    add main view

---
+++ app/views/MainView.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Костя
+ * Date: 22.09.2018
+ * Time: 14:27
+ */
+?>
+<h2 align="center">Fill in resume</h2>
+<form action="/list" method="POST">
+
+</form>
