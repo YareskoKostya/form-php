c26c0280cf21b2fbca72ea5afed12f03cbe576d5
YareskoKostya <zhtcmrj@gmail.com>
2018-09-23 16:20:00 +0300

    ch script: add check(), required

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 function addPhoto(){
     document.getElementById('imgInp').click();
 }
+function check() {
+    if (!imgInp.required)
+    {
+        alert('Add Photo');
+    }
+}
 $(document).ready( function() {
 
     function readURL(input) {
@@ -12,6 +18,7 @@ $(document).ready( function() {
             }
 
             reader.readAsDataURL(input.files[0]);
+            document.getElementById("imgInp").required = true;
         }
     }
 
@@ -25,31 +32,31 @@ function addStudy() {
         str = '<br/><div class="form-group row">' +
             '<label for="date-input" class="col-md-4 col-form-label">Start Date:</label>' +
             '<div class="col-md-8">' +
-            '<input class="form-control" type="date" name="datebeginstudy' + (x + 1) + '" id="date-input">' +
+            '<input class="form-control" type="date" name="datebeginstudy' + (x + 1) + '" id="date-input" required>' +
             '</div>' +
             '</div>' +
             '<div class="form-group row">' +
             '<label for="date-input" class="col-md-4 col-form-label">End Date:</label>' +
             '<div class="col-md-8">' +
-            '<input class="form-control" type="date" name="dateendstudy' + (x + 1) + '" id="date-input">' +
+            '<input class="form-control" type="date" name="dateendstudy' + (x + 1) + '" id="date-input" required>' +
             '</div>' +
             '</div>' +
             '<div class="form-group row">\n' +
             '<label for="text-input" class="col-4 col-form-label">Institution Name:</label>\n' +
             '<div class="col-8">\n' +
-            '<input class="form-control" type="text" name="studyname' + (x + 1) + '" id="text-input">\n' +
+            '<input class="form-control" type="text" name="studyname' + (x + 1) + '" id="text-input" required>\n' +
             '</div>\n' +
             '</div>' +
             '<div class="form-group row">\n' +
             '<label for="text-input" class="col-4 col-form-label">Speciality:</label>\n' +
             '<div class="col-8">\n' +
-            '<input class="form-control" type="text" name="professionstudy' + (x + 1) + '" id="text-input">\n' +
+            '<input class="form-control" type="text" name="professionstudy' + (x + 1) + '" id="text-input" required>\n' +
             '</div>\n' +
             '</div>\n' +
             '<div class="form-group row">\n' +
             '<label for="text-input" class="col-4 col-form-label">Academic Degree:</label>\n' +
             '<div class="col-8">\n' +
-            '<input class="form-control" type="text" name="doctor' + (x + 1) + '" id="text-input">\n' +
+            '<input class="form-control" type="text" name="doctor' + (x + 1) + '" id="text-input" required>\n' +
             '</div>\n' +
             '</div>' +
             '<div class="form-group" id="inputstudy' + (x + 1) + '">\n' +
@@ -67,25 +74,25 @@ function addWork() {
         str2 = '<br/><div class="form-group row">\n' +
             '<label for="date-input" class="col-md-4 col-form-label">Start Work:</label>\n' +
             '<div class="col-md-8">\n' +
-            '<input class="form-control" type="date" name="datebeginwork' + (y + 1) + '" id="date-input">\n' +
+            '<input class="form-control" type="date" name="datebeginwork' + (y + 1) + '" id="date-input" required>\n' +
             '</div>\n' +
             '</div>\n' +
             '<div class="form-group row">\n' +
             '<label for="date-input" class="col-md-4 col-form-label">End Work:</label>\n' +
             '<div class="col-md-8">\n' +
-            '<input class="form-control" type="date" name="dateendwork' + (y + 1) + '" id="date-input">\n' +
+            '<input class="form-control" type="date" name="dateendwork' + (y + 1) + '" id="date-input" required>\n' +
             '</div>\n' +
             '</div>\n' +
             '<div class="form-group row">\n' +
             '<label for="text-input" class="col-4 col-form-label">Company Name:</label>\n' +
             '<div class="col-8">\n' +
-            '<input class="form-control" type="text" name="workname' + (y + 1) + '" id="text-input">\n' +
+            '<input class="form-control" type="text" name="workname' + (y + 1) + '" id="text-input" required>\n' +
             '</div>\n' +
             '</div>\n' +
             '<div class="form-group row">\n' +
             '<label for="text-input" class="col-4 col-form-label">Speciality:</label>\n' +
             '<div class="col-8">\n' +
-            '<input class="form-control" type="text" name="professionwork' + (y + 1) + '" id="text-input">\n' +
+            '<input class="form-control" type="text" name="professionwork' + (y + 1) + '" id="text-input" required>\n' +
             '</div>\n' +
             '</div>\n' +
             '<div class="form-group" id="inputwork' + (y + 1) + '">\n' +
