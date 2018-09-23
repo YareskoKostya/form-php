43e6bdd4806749f702cd98f220912739d63411f9
YareskoKostya <zhtcmrj@gmail.com>
2018-09-23 09:14:53 +0300

    ch main view: add name input

--- app/views/MainView.php
+++ app/views/MainView.php
@@ -23,27 +23,27 @@
             </div>
             <div class="col-md-6">
                 <div class="form-group row">
-                    <label for="example-text-input" class="col-4 col-form-label">Your First Name:</label>
+                    <label for="text-input" class="col-4 col-form-label">Your First Name:</label>
                     <div class="col-8">
-                        <input class="form-control" type="text" id="example-text-input">
+                        <input class="form-control" type="text" name="name" id="text-input">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="example-text-input" class="col-4 col-form-label">Your Last Name:</label>
+                    <label for="text-input" class="col-4 col-form-label">Your Last Name:</label>
                     <div class="col-8">
-                        <input class="form-control" type="text" id="example-text-input">
+                        <input class="form-control" type="text" name="surname" id="text-input">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="example-date-input" class="col-md-4 col-form-label">Your Birthdate:</label>
+                    <label for="date-input" class="col-md-4 col-form-label">Your Birthdate:</label>
                     <div class="col-md-8">
-                        <input class="form-control" type="date" id="example-date-input">
+                        <input class="form-control" type="date" name="birthdate" id="date-input">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="example-date-input" class="col-md-4 col-form-label">Your Country:</label>
+                    <label for="text-input" class="col-md-4 col-form-label">Your Country:</label>
                     <div class="col-md-8">
-                        <select class="custom-select" name="country" id="example-date-input">
+                        <select class="custom-select" name="country" id="text-input">
                             <option selected></option>
                             <option>Ukraine</option>
                             <option>Belarus</option>
@@ -54,57 +54,57 @@
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="example-text-input" class="col-4 col-form-label">Your Telephone:</label>
+                    <label for="text-input" class="col-4 col-form-label">Your Telephone:</label>
                     <div class="col-8">
-                        <input class="form-control" type="tel" id="example-text-input">
+                        <input class="form-control" type="tel" name="tel" id="text-input">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="example-text-input" class="col-4 col-form-label">Your Email:</label>
+                    <label for="text-input" class="col-4 col-form-label">Your Email:</label>
                     <div class="col-8">
-                        <input class="form-control" type="email" id="example-text-input">
+                        <input class="form-control" type="email" name="email" id="text-input">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="example-text-input" class="col-4 col-form-label">Your Address:</label>
+                    <label for="text-input" class="col-4 col-form-label">Your Address:</label>
                     <div class="col-8">
-                        <input class="form-control" type="text" id="example-text-input">
+                        <input class="form-control" type="text" name="address" id="text-input">
                     </div>
                 </div>
                 <div class="form-group" align="center">
                     <h5><label class="col-12 col-form-label">Education:</label></h5>
                 </div>
                 <div class="form-group row">
-                    <label for="example-date-input" class="col-md-4 col-form-label">Start Date:</label>
+                    <label for="date-input" class="col-md-4 col-form-label">Start Date:</label>
                     <div class="col-md-8">
-                        <input class="form-control" type="date" id="example-date-input">
+                        <input class="form-control" type="date" name="datebeginstudy0"> id="date-input">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="example-date-input" class="col-md-4 col-form-label">End Date:</label>
+                    <label for="date-input" class="col-md-4 col-form-label">End Date:</label>
                     <div class="col-md-8">
-                        <input class="form-control" type="date" id="example-date-input">
+                        <input class="form-control" type="date" name="dateendstudy0" id="date-input">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="example-text-input" class="col-4 col-form-label">Institution Name:</label>
+                    <label for="text-input" class="col-4 col-form-label">Institution Name:</label>
                     <div class="col-8">
-                        <input class="form-control" type="text" id="example-text-input">
+                        <input class="form-control" type="text"name="studyname0" id="text-input">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="example-text-input" class="col-4 col-form-label">Speciality:</label>
+                    <label for="text-input" class="col-4 col-form-label">Speciality:</label>
                     <div class="col-8">
-                        <input class="form-control" type="text" id="example-text-input">
+                        <input class="form-control" type="text" name="professionstudy0" id="text-input">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="example-text-input" class="col-4 col-form-label">Academic Degree:</label>
+                    <label for="text-input" class="col-4 col-form-label">Academic Degree:</label>
                     <div class="col-8">
-                        <input class="form-control" type="text" id="example-text-input">
+                        <input class="form-control" type="text" name="doctor0" id="text-input">
                     </div>
                 </div>
-                <div class="form-group" id="">
+                <div class="form-group" id="inputstudy0">
                 </div>
                 <div class="form-group"  align="center">
                     <input type="button" name="btnSubmit" class="btnContact" value="Add More"/>
@@ -113,30 +113,30 @@
                     <h5><label class="col-12 col-form-label">Work:</label></h5>
                 </div>
                 <div class="form-group row">
-                    <label for="example-date-input" class="col-md-4 col-form-label">Start Work:</label>
+                    <label for="date-input" class="col-md-4 col-form-label">Start Work:</label>
                     <div class="col-md-8">
-                        <input class="form-control" type="date" id="example-date-input">
+                        <input class="form-control" type="date" name="datebeginwork0" id="date-input">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="example-date-input" class="col-md-4 col-form-label">End Work:</label>
+                    <label for="date-input" class="col-md-4 col-form-label">End Work:</label>
                     <div class="col-md-8">
-                        <input class="form-control" type="date" id="example-date-input">
+                        <input class="form-control" type="date" name="dateendwork0" id="date-input">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="example-text-input" class="col-4 col-form-label">Company Name:</label>
+                    <label for="text-input" class="col-4 col-form-label">Company Name:</label>
                     <div class="col-8">
-                        <input class="form-control" type="text" id="example-text-input">
+                        <input class="form-control" type="text" name="workname0" id="text-input">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="example-text-input" class="col-4 col-form-label">Speciality:</label>
+                    <label for="text-input" class="col-4 col-form-label">Speciality:</label>
                     <div class="col-8">
-                        <input class="form-control" type="text" id="example-text-input">
+                        <input class="form-control" type="text" name="professionwork0" id="text-input">
                     </div>
                 </div>
-                <div class="form-group" id="">
+                <div class="form-group" id="inputwork0">
                 </div>
                 <div class="form-group"  align="center">
                     <input type="button" name="btnSubmit" class="btnContact" value="Add More"/>
