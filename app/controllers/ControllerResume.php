94643f6917e7435fb4fff39bf6de6043cc68e72b
YareskoKostya <zhtcmrj@gmail.com>
2018-09-25 20:34:24 +0300

    add controller resume

---
+++ app/controllers/ControllerResume.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Костя
+ * Date: 25.09.2018
+ * Time: 20:21
+ */
+
+namespace App\Controllers;
+
+use App\Core\Controller;
+use App\Models\ModelResume;
+use App\Core\View;
+
+class ControllerResume extends Controller
+{
+    function __construct($settings) {
+        parent::__construct();
+        $this->model = new ModelResume($settings);
+        $this->view = new View();
+    }
+
+    function action_index() {
+        $data = $this->model->getData();
+        $this->view->generate('ResumeView.php', $data);
+    }
+}
